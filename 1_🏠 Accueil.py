a83eb61706e37485a286eb94459b630347a1c7d8
YacineAyachi <101209432+YacineAyachi@users.noreply.github.com>
2024-11-09 23:31:28 +0100

    Delete 1_🏠 Accueil.py

--- 1_🏠 Accueil.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("m2-enedis")
-
-st.write("Bienvenue sur la page d'accueil ! Utilisez le menu à gauche pour naviguer entre les pages.")
-
-st.image("objects/image.jpg")
-
-st.write("done")
