cf05137ddf444f1c35271c30d355fb529295d083
YacineAyachi <101209432+YacineAyachi@users.noreply.github.com>
2024-11-09 23:19:54 +0100

    image added

--- 1_🏠 Accueil.py
+++ 1_🏠 Accueil.py
@@ -4,6 +4,6 @@ st.title("Mon Application Web")
 
 st.write("Bienvenue sur l'application principale ! Utilisez le menu à gauche pour naviguer entre les pages.")
 
-st.image("objects/image.jpg")
+st.image("objects/image")
 
 st.write("done")
