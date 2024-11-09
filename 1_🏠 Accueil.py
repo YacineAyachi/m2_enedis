c292cdee25e3ad8c6abbb9c8dba7f4017de28016
YacineAyachi <101209432+YacineAyachi@users.noreply.github.com>
2024-11-09 23:28:19 +0100

    Create 1_🏠 Accueil.py

---
+++ 1_🏠 Accueil.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("m2-enedis")
+
+st.write("Bienvenue sur la page d'accueil ! Utilisez le menu à gauche pour naviguer entre les pages.")
+
+st.image("objects/image.jpg")
+
+st.write("done")
